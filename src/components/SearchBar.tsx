27ab8f4d5d44a37c083c367d927327ec80cdfb32
Norbert Boian <norbertboian@gmail.com>
2023-12-12 23:32:11 +0200

    disabled browser autocomplete for the input to avoid overlaying over results

--- src/components/SearchBar.tsx
+++ src/components/SearchBar.tsx
@@ -167,6 +167,7 @@ export const SearchBar = () => {
           ref={inputRef}
           value={value}
           onChange={handleOnChange}
+          autocomplete={false}
           aria-autocomplete="list"
           aria-expanded={visibleResultsList}
           aria-controls="listbox"
