a594eb1777a4002cfd60407777733213a9c6e56c
Norbert Boian <norbertboian@gmail.com>
2023-12-12 23:45:41 +0200

    fixed incomplete inline style in App.tsx

--- src/App.tsx
+++ src/App.tsx
@@ -3,7 +3,7 @@ import { SearchBar } from "./components/SearchBar";
 
 function App() {
   return (
-    <div style={{ width: "clamp(20rem, 50vw, 50re" }}>
+    <div style={{ width: "clamp(20rem, 50vw, 50rem)" }}>
       <SearchBar />
     </div>
   );
